d43dd4ede9d235ef2566f01e45e0c1549aaf735b
Dennis-SID <kilimanjjjaro@gmail.com>
2021-08-10 22:02:12 +0200

    project layer conflict

--- Stanford CS 106A/src/week2/dsidelnik/assignment2/Assignment2Part2.java
+++ Stanford CS 106A/src/week2/dsidelnik/assignment2/Assignment2Part2.java
@@ -3,6 +3,6 @@ package week2.dsidelnik.assignment2;
 public class Assignment2Part2 {
 
     public void run() {
-        
+
     }
 }
---
+++ Stanford CS 106A/src/week2/dsidelnik/assignment2/Assignment2Part3.java
@@ -0,0 +1,7 @@
+package week2.dsidelnik.assignment2;
+
+public class Assignment2Part3 {
+    public static void main (String [] args) {
+        System.out.println("TEXT");
+    }
+}
