35c761744b675721ce02a5a6d3196a817a238a76
Dennis-SID <kilimanjjjaro@gmail.com>
2021-08-11 23:48:28 +0200

    third task started

---
+++ Stanford CS 106A/src/com/cscourse/week3/dsidelnik/assignment3/Assignment3Part3.java
@@ -0,0 +1,22 @@
+package com.cscourse.week3.dsidelnik.assignment3;
+
+import com.shpp.cs.a.console.TextProgram;
+
+/**
+ * <strong>Assignment3 Part3</strong>
+ * <p>Returns the value of the first argument  raised to hte power of the second argument
+ * special cases:
+ * - if the second argument is positive or negative zero, then the result is 1.0;
+ * - if the second argument is 1.0 than the result is the same as first argument</p>
+ * - second parameter could be a negative value
+ *
+ * to make method less complex all that exponents to a zero power will equal to one
+ */
+public class Assignment3Part3 extends TextProgram {
+
+    public void run() {
+        
+    }
+
+
+}
