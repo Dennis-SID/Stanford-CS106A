295a8b74b6fae62f6dc3c65ca8ca1390479c8ace
Dennis-SID <kilimanjjjaro@gmail.com>
2021-08-18 23:55:38 +0200

    starting working on actionlisteners

--- Stanford CS 106A/src/com/cscourse/week4/dsidelnik/assignment4/Assignment4Part1.java
+++ Stanford CS 106A/src/com/cscourse/week4/dsidelnik/assignment4/Assignment4Part1.java
@@ -4,61 +4,97 @@ import acm.graphics.*;
 import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Assignment4Part1 extends WindowProgram {
-    /** Width and height of application window in pixels */
+public class Assignment4Part1 extends WindowProgram implements MouseMotionListener {
+    /**
+     * Width and height of application window in pixels
+     */
     public static final int APPLICATION_WIDTH = 400;
     public static final int APPLICATION_HEIGHT = 600;
 
-    /** Dimensions of game board (usually the same) */
+    /**
+     * Dimensions of game board (usually the same)
+     */
     private static final int WIDTH = APPLICATION_WIDTH;
     private static final int HEIGHT = APPLICATION_HEIGHT;
 
-    /** Dimensions of the paddle */
+    /**
+     * Dimensions of the paddle
+     */
     private static final int PADDLE_WIDTH = 60;
     private static final int PADDLE_HEIGHT = 10;
 
-    /** Offset of the paddle up from the bottom */
+    /**
+     * Offset of the paddle up from the bottom
+     */
     private static final int PADDLE_Y_OFFSET = 30;
 
-    /** Number of bricks per row */
+    /**
+     * Number of bricks per row
+     */
     private static final int NBRICKS_PER_ROW = 10;
 
-    /** Number of rows of bricks */
+    /**
+     * Number of rows of bricks
+     */
     private static final int NBRICK_ROWS = 10;
 
-    /** Separation between bricks */
+    /**
+     * Separation between bricks
+     */
     private static final int BRICK_SEP = 4;
 
-    /** Width of a brick */
+    /**
+     * Width of a brick
+     */
     private static final int BRICK_WIDTH =
             (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
-    /** Height of a brick */
+    /**
+     * Height of a brick
+     */
     private static final int BRICK_HEIGHT = 8;
 
-    /** Radius of the ball in pixels */
+    /**
+     * Radius of the ball in pixels
+     */
     private static final int BALL_RADIUS = 10;
 
-    /** Offset of the top brick row from the top */
+    /**
+     * Offset of the top brick row from the top
+     */
     private static final int BRICK_Y_OFFSET = 70;
 
-    /** Number of turns */
+    /**
+     * Number of turns
+     */
     private static final int NTURNS = 3;
 
 
     public void run() {
-        endGameWin();
-        /*for (int i = 0; i < NTURNS; i++) {
+
+        for (int i = 0; i < NTURNS; i++) {
             playGame();
-        }*/
+        }
+        endGame(1);
     }
 
     private void playGame() {
         GRect paddle = drawPaddle();
         GOval ball = drawBall();
         drawAllBrickLevels();
+
+        for (int i = 0; i < NTURNS; i++) {
+            paddle.addActionListener(mouseMoved(MouseEvent e) {
+
+            };
+        }
     }
+
 
     private void drawBrick(double x, double y, Color color) {
         GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
@@ -96,6 +132,7 @@ public class Assignment4Part1 extends WindowProgram {
         paddle.setVisible(true);
         paddle.setFilled(true);
         paddle.setFillColor(Color.BLACK);
+        paddle.addActionListener(this);
         add(paddle);
         return paddle;
     }
@@ -118,54 +155,67 @@ public class Assignment4Part1 extends WindowProgram {
         Color outputColor;
 
         switch (rowNumber) {
-            case (0) :
-            case (1) : outputColor = Color.RED;
-            break;
-            case (2) :
-            case (3) : outputColor = Color.ORANGE;
-            break;
-            case (4) :
-            case (5) : outputColor = Color.YELLOW;
-            break;
-            case (6) :
-            case (7) : outputColor = Color.GREEN;
-            break;
-            case (8) :
-            case (9) : outputColor = Color.CYAN;
-            break;
-            default : outputColor = Color.WHITE;
+            case (0):
+            case (1):
+                outputColor = Color.RED;
+                break;
+            case (2):
+            case (3):
+                outputColor = Color.ORANGE;
+                break;
+            case (4):
+            case (5):
+                outputColor = Color.YELLOW;
+                break;
+            case (6):
+            case (7):
+                outputColor = Color.GREEN;
+                break;
+            case (8):
+            case (9):
+                outputColor = Color.CYAN;
+                break;
+            default:
+                outputColor = Color.WHITE;
         }
         return outputColor;
     }
 
-    private void endGameWin() {
+    private void endGame(int winLose) {
         int fontSize = 60;
         int animationFrame = 20;
+        GLabel resultLabel;
 
-        GLabel winLabel = new GLabel("VICTORY!!!");
-        winLabel.setVisible(true);
-        winLabel.setColor(Color.RED);
+        if (winLose == 1)
+            resultLabel = new GLabel("VICTORY!!!");
+        else
+            resultLabel = new GLabel("GAME OVER!!!");
+
+        resultLabel.setVisible(true);
+        resultLabel.setColor(Color.RED);
 
 
         for (int i = 0; i < fontSize; i += 3) {
-            double labelX = getWidth() / 2.0 - winLabel.getWidth() / 2.0;
-            double labelY = getHeight() / 2.0 - winLabel.getHeight() / 2.0;
+            double labelX = getWidth() / 2.0 - resultLabel.getWidth() / 2.0;
+            double labelY = getHeight() / 2.0 - resultLabel.getHeight() / 2.0;
             Font font = new Font(Font.SANS_SERIF, Font.BOLD, i);
-            winLabel.setFont(font);
-            winLabel.setLocation(labelX, labelY);
-            add(winLabel);
+            resultLabel.setFont(font);
+            resultLabel.setLocation(labelX, labelY);
+            add(resultLabel);
             pause(animationFrame);
         }
 
-
-
-
-
-
-
-
-
-
     }
 
- }
+    public void mouseDragged(MouseEvent e) {}
+
+    /**
+     * Invoked when the mouse cursor has been moved onto a component
+     * but no buttons have been pushed.
+     */
+    public void mouseMoved(MouseEvent e) {
+        System.out.println(e.getX());
+    }
+
+
+}
